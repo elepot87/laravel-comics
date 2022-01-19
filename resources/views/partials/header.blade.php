3a19b1d9505f2c65b39e7c1e6868d73cdcf0e3d8
elepot87 <78643839+elepot87@users.noreply.github.com>
2022-01-19 17:06:46 +0100

    aggiunta classe active con @if - fizx style

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,10 +5,26 @@
       </a>
       <nav class="nav-menu">
         <ul class="main-menu flex no-list-style">
-            <li class="menu-item active"><a href="{{route('comics')}}">Comics</a></li>
-            <li class="menu-item"><a href="{{route('news')}}">News</a></li>
+            <li class="menu-item">
+                <a href="{{route('comics')}}" 
+                @if (Request::route()->getName() === 'comics')
+                    class="item-active"
+                @endif
+                >
+                    Comics
+                </a>
+            </li>
+            <li class="menu-item">
+                <a href="{{route('news')}}"
+                @if (Request::route()->getName() === 'news')
+                    class="item-active"
+                @endif
+                >
+                    News
+                </a>
+            </li>
         </ul>
     </nav>
-    @dump(Request::route()->getName())
+    {{-- @dump(Request::route()->getName()) --}}
  </div>
 </header>
