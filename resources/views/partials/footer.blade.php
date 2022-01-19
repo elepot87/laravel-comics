2342352a50846af62e902433139e20c367b02f7b
elepot87 <78643839+elepot87@users.noreply.github.com>
2022-01-19 19:02:05 +0100

    aggiunto footer e stile

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,24 @@
 <footer>
-    questo è il footer
+    <div class="footer-social">       
+        <div class="container py-20 flex flex-between">
+            <!-- Col left -->
+            <button class="footer-cta">SIGN-UP NOW</button>
+            <!-- Col right -->
+        <div class="flex flex-center">
+          <span class="follow">FOLLOW US</span>
+          <div class="container-icon">
+            <img src="{{asset('images/footer-facebook.png')}}" alt="icona facebook" />
+            <img src="{{asset('images/footer-twitter.png')}}" alt="icona twitter" />
+            <img src="{{asset('images/footer-youtube.png')}}" alt="icona-youtube" />
+            <img
+              src="{{asset('images/footer-pinterest.png')}}"
+              alt="icona pinterest"
+            />
+            <img
+              src="{{asset('images/footer-periscope.png')}}"
+              alt="icona periscope"
+            />
+          </div>
+        </div>    
+    </div>
 </footer>
