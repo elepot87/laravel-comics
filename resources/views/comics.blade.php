21d830c50d68aaec56785ef979e464a40e836edd
elepot87 <78643839+elepot87@users.noreply.github.com>
2022-01-19 17:35:09 +0100

    parte di mark up e stile per comics.blade

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,5 +1,20 @@
 @extends('layouts.main')
 
 @section('main-content')
-    <h1>Pagina comics</h1>
+    <div class="products">
+        <div class="container flex">
+            {{-- <div class="title">
+                <h1 class="text-title">Current series</h1>
+            </div> --}}
+            
+            <ul class="container-comics flex no-list-style">
+                <li>
+                    ciao
+          {{-- <Comics :url="comic.thumb" :title="comic.series" /> --}}
+                </li>
+                <li>ciao</li>
+            </ul>
+            <button class="loader">Load more</button>
+        </div>
+    </div>
 @endsection
