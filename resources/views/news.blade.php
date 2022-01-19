1469c9e528247a9bed9cda7cb0973780da6496e9
elepot87 <78643839+elepot87@users.noreply.github.com>
2022-01-19 16:28:26 +0100

    @extends anche per pagina Comics e NEWS

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.main')
+
+@section('main-content')
     <h1>Pagina news</h1>
-</body>
-</html>
+@endsection
