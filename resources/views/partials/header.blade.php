8cbc592749d57ad2ba93eadc32fa3056ee436619
elepot87 <78643839+elepot87@users.noreply.github.com>
2022-01-19 16:44:51 +0100

    aggiunti file partial/_scss

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,12 +1,13 @@
 <header>
-
-    <a href="{{route('home')}}">
-        <img src="{{asset('images/dc-logo.png')}}" alt="dc logo">
-    </a>
-    <nav>
-        <ul>
+    <div class="container flex flex-center">
+      <a href="{{route('home')}}">
+        <img src="{{asset('images/dc-logo.png')}}" alt="DC logo" />
+      </a>
+      <nav class="nav-menu">
+        <ul class="main-menu flex no-list-style">
             <li><a href="{{route('comics')}}">Comics</a></li>
             <li><a href="{{route('news')}}">News</a></li>
         </ul>
     </nav>
+ </div>
 </header>
