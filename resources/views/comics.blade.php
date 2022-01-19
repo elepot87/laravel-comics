f698cc2bf53fb6ac3b0b8fe5bdb309f1a5e57905
elepot87 <78643839+elepot87@users.noreply.github.com>
2022-01-19 18:25:35 +0100

    foreach per comics

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -6,15 +6,22 @@
             {{-- <div class="title">
                 <h1 class="text-title">Current series</h1>
             </div> --}}
-            
             <ul class="container-comics flex no-list-style">
-                <li>
-                    ciao
-          {{-- <Comics :url="comic.thumb" :title="comic.series" /> --}}
-                </li>
-                <li>ciao</li>
+               <li>
+                   @foreach ($comics as $comic)
+                   <div class="card">
+                        <div class="container-img">
+                            <img src="{{$comic['thumb']}}" alt="{{$comic['title']}}" />
+                        </div>
+                        <div class="title-comics">
+                            {{$comic['title']}}
+                        </div>
+                    </div>                    
+                   @endforeach
+               </li>
             </ul>
             <button class="loader">Load more</button>
         </div>
     </div>
+  
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,13 @@
     <meta name="viewport" content="width=h1, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>DC Comics</title>
+    {{-- Google Font --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Yanone+Kaffeesatz:wght@400;500;600;700&display=swap" rel="stylesheet"> 
+    {{-- Main style --}}
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    
 </head>
 <body>
     @include('partials.header')
